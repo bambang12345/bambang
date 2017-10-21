2444084caaca1b8445cf8cc97a2c5b7d7e953467
bambang12345 <32986739+bambang12345@users.noreply.github.com>
2017-10-22 02:31:13 +0700

    Add files via upload

--- post3.js
+++ post3.js
@@ -1,1 +1,58 @@
-https://linkservicersphones.blogspot.co.id/p/smartphone.html
+<div class="text-xs-center margin-bottom-20">
+<a href="https://yllix.com/publishers/732174" target="_blank"><img src="https://yllix.com/images/aff/pub_upk7x1.jpg" width="728" height="90" alt="Monetize your website traffic with yX Media" title="Monetize your website traffic with yX Media" /></a>
+  </div>
+  <div class="col-md-6 float-xs-left">
+<div class="card">
+    <div class="card-header text-xs-center"><h2><i class="fa fa-shield"></i> Safe Link Converter <i class="fa fa-shield"></i></h2></div>
+        <div class="card-block text-xs-center">
+          <p class="text-info margin-bottom-20">Encrypting your link and protect the link from viruses, malware, thief, etc!<br/>Made your link safe to visit.</p>
+<div class="progress" id="daplong">
+    <progress class="progress progress-striped progress-info progress-animated" value="0" max="100" data-percentage="100"></progress>
+</div>
+<button id="download2" class="alert alert-dismissible alert-success btn-lg" style='display:none'><i class="fa fa-thumbs-o-up " aria-hidden="true"></i> <strong>Well done!</strong> you have successfully gained access to Decrypted Link. <i class="fa fa-hand-o-down slideInDownThumb" aria-hidden="true"></i></button>
+        </div>
+</div>
+</div>
+
+Bla...bla...bla...bla.<br />
+<br />
+Bla...bla...bla...bla.<br />
+<br />
+Bla...bla...bla...bla.<br />
+<br />
+Bla...bla...bla...bla.<br />
+<br />
+
+<div class="col-md-4 margin-bottom-20 float-xs-right text-xs-center">
+<h3>Advertise</h3>
+<a href="https://yllix.com/publishers/732174" target="_blank"><img src="https://yllix.com/images/aff/pub_7nc2s6.jpg" width="300" height="250" alt="yX Media - Monetize your website traffic with us" title="yX Media - Monetize your website traffic with us" /></a>
+</div>
+
+Bla...bla...bla...bla.<br />
+<br />
+Bla...bla...bla...bla.<br />
+<br />
+Bla...bla...bla...bla.<br />
+<br />
+Bla...bla...bla...bla.<br />
+<br />
+
+<div class="col-md-4 margin-bottom-20 float-xs-left text-xs-center">
+<h3>Advertise</h3>
+<a href="https://yllix.com/publishers/732174" target="_blank"><img src="https://yllix.com/images/aff/pub_s9c2nm.jpg" width="300" height="250" alt="Monetize your website traffic with yX Media" title="Monetize your website traffic with yX Media" /></a>
+<br/>
+<br/>
+<script src="https://cdn.rawgit.com/KompiAjaib/js/master/post1.js" type="text/javascript"></script>
+<script type="text/javascript">
+var currentURL=location.href;
+var str = currentURL;
+var res = str.replace("https://kompisafelinkv2.blogspot.com/2016/12/the-way-to-start-business-at-home.html?url=", "");
+document.write('<button type="button" id="download" class="Visit_Link btn btn-success" onclick="changeLink();" style="display: none;"><strong>Visit Link</strong> <i class="fa fa-external-link" aria-hidden="true"></i></button>')
+</script>
+</div>
+
+What is recovery?  In Android, recovery refers to the dedicated, bootable partition that has the recovery console installed.  A combination of key presses (or instructions from a command line) will boot your phone to recovery, where you can find tools to help repair (recover) your installation as well as install official OS updates. Because Android is open and has the recovery source code available, building a customized version with more and different options is relatively easy as well. Let's look at both options.
+
+The stock recovery is pretty limited, but that's by design.  Its main purpose is to delete all or some user data and files, or to perform system updates manually with signed and verified delta update packages. Normally, both these operations are started from the running Android system, but you can do things manually and boot right into recovery yourself.  When you tell your phone to do a factory reset, recovery is what boots up and erases the files and data. Likewise with updates— when we restart to install an official OS update, it's done in recovery. Recovery is also where we go to manually install official OS updates we've downloaded from the Internet. It's very useful, but limited. Different manufacturers use different recovery methods but all perform the same basic functions — provide the tools needed to recovery a working system.
+
+Custom Android recoveries offer much more. They have been coded to allow for backup and restore functions, selective deletion of data so you don't have to wipe everything, and modified to allow update packages that have not been digitally signed by official sources. You also can mount various partitions so that you can copy files to the SD card without having to remove it or reboot into Android. Because of the extra functionality built in, they are a pretty important tool for folks who want to hack their Android phone or tablet. Recoveries aren't as pretty as a custom ROM and don't get the same love from users that custom builds of Android do, but in the end they're even more important. Without them none of this custom ROM stuff would be possible. 
