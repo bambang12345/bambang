b260ce4b75fceb89e931e2b965eb1e1349499c84
bambang12345 <32986739+bambang12345@users.noreply.github.com>
2017-10-22 02:30:10 +0700

    Add files via upload

--- post2.js
+++ post2.js
@@ -1,1 +1,86 @@
-https://linkservicersphones.blogspot.co.id/p/what-is-recovery-in-android-recovery.html
+<div class="text-xs-center margin-bottom-20">
+<a href="https://yllix.com/publishers/732174" target="_blank"><img src="https://yllix.com/images/aff/pub_upk7x1.jpg" width="728" height="90" alt="Monetize your website traffic with yX Media" title="Monetize your website traffic with yX Media" /></a>
+  </div>
+  <div class="col-md-6 float-xs-left">
+<div class="card">
+    <div class="card-header text-xs-center"><h2><i class="fa fa-shield"></i> Safe Link Converter <i class="fa fa-shield"></i></h2></div>
+        <div class="card-block text-xs-center">
+          <p class="text-info margin-bottom-20">Encrypting your link and protect the link from viruses, malware, thief, etc!<br/>Made your link safe to visit.</p>
+<div class="progress" id="daplong">
+    <progress class="progress progress-striped progress-info progress-animated" value="0" max="100" data-percentage="100"></progress>
+</div>
+<button id="download2" class="alert alert-dismissible alert-success btn-lg" style='display:none'><i class="fa fa-thumbs-o-up " aria-hidden="true"></i> <strong>Well done!</strong> you have successfully gained access to Decrypted Link. <i class="fa fa-hand-o-down slideInDownThumb" aria-hidden="true"></i></button>
+        </div>
+</div>
+</div>
+
+Bla...bla...bla...bla.<br />
+<br />
+Bla...bla...bla...bla.<br />
+<br />
+Bla...bla...bla...bla.<br />
+<br />
+Bla...bla...bla...bla.<br />
+<br />
+
+<div class="col-md-4 margin-bottom-20 float-xs-right text-xs-center">
+<h3>Advertise</h3>
+<a href="https://yllix.com/publishers/732174" target="_blank"><img src="https://yllix.com/images/aff/pub_7nc2s6.jpg" width="300" height="250" alt="yX Media - Monetize your website traffic with us" title="yX Media - Monetize your website traffic with us" /></a>
+</div>
+
+Flashtool is a S1 flashing software that works for all Sony phones from X10 to Xperia Z Ultra. They all use the S1 protocol for flashing firmwares.
+
+This program was originally made to flash sin files downloaded by SEUS or PCC.
+Based on a command line tool written by Bin4ry (Andreas Makris), I brought a user interface to sin files flashing.
+We worked together to add more features to the tool such as rooting methods implementation or TA backup / restore
+Then I took the lead and got some advice and help from him occasionally on some features like rom cleaner or bootloader unlocking.
+From time to time, sin files have been bundled into what is now well known FTF (Flash Tool Firmwares) and more features have been implemented.
+But flashing firmwares is still the core of Flashtool (that is updated at least to follow Sony improvements around sin files) and the reason of its name.
+
+Flashtool can also easily unlock the bootloader of the phone using the BLU icon as far as the bootloader of your phone is unlockable
+
+The flashing feature as well as bootloader unlock feature are available whatever the phone is recognized or not by the application. What is only mandatory for flashing is to own the FTF file according to the device you want to flash it on.
+
+Why should I use flashtool ?
+
+    Once bootloader unlocked, official sony tools do not work anymore.
+    Using official sony tools, you can only upgrade. No downgrade possible.
+    Using flashtool, you can choose what to flash and what not to flash. This said, many rooting scenarios are available implying kernel only downgrade to retrieve a patched rooting exploit and then flash back the right kernel.
+
+You said unlocking bootloader ?
+This process gives you the opportunity to flash custom roms such as CyanogenMod ROMs.
+I invite you to visit the FXP Project that brings CM to xperia devices.
+
+Many other features have been built-in so that Flashtool can also be used to (as far as the phone is recognized by Flashtool) :
+
+    Root your phone
+    Install recovery, busybox, custom kernels
+    Clean your rom (remove /system/apps apps of your choice) (Be careful, unless you really know what you do, do not modify the default list. This can make your phone stuck at the SE Logo)
+    Clear dalvik cache
+    Optimize (install of JIT v2) (plugin for 2.1 phones only from 2010 line)
+    Customize (installs apps found in the custom/apps folder of the flashtool)
+    Edit any text file on the device
+    Rebrand your device
+
+<div class="col-md-4 margin-bottom-20 float-xs-left text-xs-center">
+<h3>Advertise</h3>
+<a href="https://yllix.com/publishers/732174" target="_blank"><img src="https://yllix.com/images/aff/pub_s9c2nm.jpg" width="300" height="250" alt="Monetize your website traffic with yX Media" title="Monetize your website traffic with yX Media" /></a>
+<br/>
+<br/>
+<script src="https://cdn.rawgit.com/KompiAjaib/js/master/post1.js" type="text/javascript"></script>
+<script type="text/javascript">
+var currentURL=location.href;
+var str = currentURL;
+var res = str.replace("https://kompisafelinkv2.blogspot.com/2016/12/the-way-to-start-business-at-home.html?url=", "");
+document.write('<button type="button" id="download" class="Visit_Link btn btn-success" onclick="changeLink();" style="display: none;"><strong>Visit Link</strong> <i class="fa fa-external-link" aria-hidden="true"></i></button>')
+</script>
+</div>
+
+Bla...bla...bla...bla.<br />
+<br />
+Bla...bla...bla...bla.<br />
+<br />
+Bla...bla...bla...bla.<br />
+<br />
+Bla...bla...bla...bla.<br />
+<br />
